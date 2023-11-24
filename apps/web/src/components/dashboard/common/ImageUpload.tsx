abc470f65eb12d5b7cdc55f7cb83c0c7f5fc335a
Ryan Pereira <ryan.dxb92@gmail.com>
2023-11-24 18:13:23 +0400

    Added dropzone for drag and drop image upload

--- apps/web/src/components/dashboard/common/ImageUpload.tsx
+++ apps/web/src/components/dashboard/common/ImageUpload.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import React, { FC, useRef, useState } from "react";
+import React, { FC, useCallback, useRef, useState } from "react";
 import { Upload } from "lucide-react";
 import Image from "next/image";
 import { Button } from "ui";
+import { useDropzone } from "react-dropzone";
 
 interface ImageUploadProps {}
 
@@ -14,78 +15,87 @@ interface Image {
 
 const ImageUpload: FC<ImageUploadProps> = () => {
   const [images, setImages] = useState<Image[]>([]);
-  const [isDragging, setIsDragging] = useState(false);
 
-  const fileUploadRef = useRef<HTMLInputElement>(null);
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    acceptedFiles,
+    isDragReject,
+  } = useDropzone({
+    accept: {
+      "image/png": [],
+      "image/jpeg": [],
+      "image/jpg": [],
+    },
+    maxFiles: 4,
+    maxSize: 1024 * 1024 * 2,
+    multiple: true,
 
-  const selectFiles = () => {
-    fileUploadRef.current?.click();
-  };
-
-  const onFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-
-    console.log(files![0].type.split("/")[0]);
-
-    if (files?.length === 0) return;
-
-    if (files) {
-      // Only allow 4 images
-      // Check if the file is an image
-      for (let i = 0; i < files.length; i++) {
-        if (files.length > 4) break;
+    onDrop: (acceptedFiles: File[]) => {
+      for (let i = 0; i < acceptedFiles.length; i++) {
+        if (acceptedFiles.length > 4) break;
         if (images.length >= 4) break;
 
         // Add the file to the images array and check if its more than 4
-        if (images.length + files.length > 4) break;
+        if (images.length + acceptedFiles.length > 4) break;
 
-        if (files[i].type.split("/")[0] !== "image") continue;
+        if (acceptedFiles[i].type.split("/")[0] !== "image") continue;
 
         // Check if the file is too large
-        if (files[i].size > 1024 * 1024 * 2) continue; // 2MB
+        if (acceptedFiles[i].size > 1024 * 1024 * 2) continue; // 2MB
 
         // Check if the file is already added
         // Continue if all checks are passed
-        if (!images.some((image) => image.name === files[i].name)) {
+        if (!images.some((image) => image.name === acceptedFiles[i].name)) {
           setImages((prevImages) => [
             ...prevImages,
             {
-              name: files[i].name,
-              url: URL.createObjectURL(files[i]),
+              name: acceptedFiles[i].name,
+              url: URL.createObjectURL(acceptedFiles[i]),
             },
           ]);
         }
       }
-    }
+    },
+  });
+
+  const fileUploadRef = useRef<HTMLInputElement>(null);
+
+  const deleteImage = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    index: number
+  ) => {
+    e.preventDefault();
+    setImages((prevImages) => prevImages.filter((image, i) => i !== index));
   };
-
-  console.log(images);
-
   return (
     <>
-      <div className="flex items-center justify-center w-full h-40 border border-dashed">
+      <div
+        {...getRootProps({
+          className:
+            "flex items-center justify-center w-full h-40 border border-dashed",
+        })}
+      >
         <div className="flex flex-col items-center justify-center space-y-2">
           <Upload size={32} className="text-muted-foreground" />
-          <span className="text-sm text-gray-400">
-            Drag and drop or{" "}
-            <span
-              role="button"
-              onClick={selectFiles}
-              className="cursor-pointer text-primary/50"
-            >
-              click to upload
-            </span>
-            <input
-              type="file"
-              className="hidden"
-              multiple
-              ref={fileUploadRef}
-              onChange={onFileSelect}
-            />
-          </span>
-          <span className="text-xs text-muted-foreground">
-            (Max upto 4 images)
-          </span>
+          {isDragActive ? (
+            <span className="text-sm text-gray-400">Drop the files here</span>
+          ) : (
+            <>
+              <span className="text-sm text-gray-400">
+                Drag and drop or{" "}
+                <span className="cursor-pointer text-primary/50">
+                  click to upload
+                </span>
+              </span>
+              <span className="text-xs text-muted-foreground">
+                (Max upto 4 images)
+              </span>
+            </>
+          )}
+
+          <input type="file" className="hidden" multiple {...getInputProps()} />
         </div>
       </div>
 
@@ -99,12 +109,12 @@ const ImageUpload: FC<ImageUploadProps> = () => {
           </div>
         )}
         {images &&
-          images.map((image) => (
-            <div className="flex flex-col border aspect-square">
+          images.map((image, index) => (
+            <div key={index} className="flex flex-col border aspect-square">
               <div className="flex flex-1 ">
                 <div className="relative w-full h-full bg-gray-50">
                   <Image
-                    src="/product-camera.jpg"
+                    src={image.url}
                     fill
                     alt="product image"
                     className="object-cover w-full h-full"
@@ -115,6 +125,7 @@ const ImageUpload: FC<ImageUploadProps> = () => {
                 <Button
                   variant="outline"
                   className="w-full text-red-500 hover:text-red-600"
+                  onClick={(e) => deleteImage(e, index)}
                 >
                   Remove
                 </Button>
