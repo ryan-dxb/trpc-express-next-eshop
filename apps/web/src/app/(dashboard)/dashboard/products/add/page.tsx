bccf1ace7dc512ea938737e362208713a2af9027
Ryan Pereira <ryan.dxb92@gmail.com>
2023-11-24 18:46:56 +0400

    Added Category Dropdown and Pricing section to add product form

--- apps/web/src/app/(dashboard)/dashboard/products/add/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/products/add/page.tsx
@@ -4,8 +4,9 @@ import PageHeader from "@/components/dashboard/common/PageHeader";
 import RichTextEditor from "@/components/dashboard/common/richTextEditor";
 import { NextPage } from "next";
 import Image from "next/image";
-import { Button, Input, Label } from "ui";
+import { Button, Checkbox, Input, Label, Separator, Switch } from "ui";
 import ImageUpload from "@/components/dashboard/common/ImageUpload";
+import CategoryDropDown from "@/components/dashboard/common/CategoryDropDown";
 
 interface AddNewProductPageProps {}
 
@@ -17,67 +18,115 @@ const AddNewProductPage: NextPage<AddNewProductPageProps> = () => {
         subTitle="Add new product to your store"
       />
       <PageContentLayout>
-        <div className="grid grid-cols-3 gap-x-8">
-          <div className="col-span-2">
-            <div className="w-full p-4 border">
-              <h3 className="text-lg font-semibold">Product Information</h3>
-              <form action="" className="flex flex-col gap-6 mt-6">
-                <InputWithLabel
-                  name="name"
-                  label="Product Name"
-                  placeholder="Enter product name"
-                />
-
-                <div className="flex flex-row space-x-8 ">
+        <form>
+          <div className="grid grid-cols-3 gap-x-8">
+            <div className="col-span-2">
+              <div className="w-full p-4 border">
+                <h3 className="text-lg font-semibold">Product Information</h3>
+                <div className="flex flex-col gap-6 mt-6">
                   <InputWithLabel
-                    label="SKU"
-                    name="sku"
-                    placeholder="Enter product SKU"
+                    name="name"
+                    label="Product Name"
+                    placeholder="Enter product name"
                   />
+
+                  <div className="flex flex-row space-x-8 ">
+                    <InputWithLabel
+                      label="SKU"
+                      name="sku"
+                      placeholder="Enter product SKU"
+                    />
+                    <InputWithLabel
+                      label="Product Id"
+                      name="productId"
+                      placeholder="Enter product id"
+                    />
+                  </div>
+
                   <InputWithLabel
-                    label="Product Id"
-                    name="productId"
-                    placeholder="Enter product id"
+                    type="textarea"
+                    label="Short Description"
+                    name="shortDescription"
+                    placeholder="Describe your product in a few words"
                   />
+
+                  {/* Text Editor for Long Description */}
+
+                  <div>
+                    <Label
+                      htmlFor="longDescription"
+                      className="text-xs font-medium uppercase text-muted-foreground"
+                    >
+                      Product Description
+                    </Label>
+                    <RichTextEditor />
+                  </div>
+
+                  {/* Product Images */}
+                  <div>
+                    <Label className="text-xs font-medium uppercase text-muted-foreground">
+                      Product Images
+                    </Label>
+
+                    <ImageUpload />
+                  </div>
                 </div>
+              </div>
+            </div>
 
-                <InputWithLabel
-                  type="textarea"
-                  label="Short Description"
-                  name="shortDescription"
-                  placeholder="Describe your product in a few words"
-                />
+            <div className="col-span-1">
+              <div className="w-full p-4 border">
+                <h3 className="text-lg font-semibold">Pricing</h3>
 
-                {/* Text Editor for Long Description */}
+                <div className="flex flex-col gap-6 mt-6">
+                  <InputWithLabel
+                    label="Base Price"
+                    name="basePrice"
+                    placeholder="Enter product base price"
+                  />
 
-                <div>
-                  <Label
-                    htmlFor="longDescription"
-                    className="text-xs font-medium uppercase text-muted-foreground"
-                  >
-                    Product Description
-                  </Label>
-                  <RichTextEditor />
+                  <InputWithLabel
+                    label="Discounted Price"
+                    name="discountedPrice"
+                    placeholder=" Enter product discounted price"
+                  />
+
+                  <Separator />
+
+                  <div className="flex flex-row justify-between">
+                    <Label
+                      htmlFor="taxable"
+                      className="text-xs font-medium text-gray-600 uppercase"
+                    >
+                      Is product taxable?
+                    </Label>
+                    <Checkbox id="taxable" className="rounded" />
+                  </div>
+                  <div className="flex flex-row justify-between">
+                    <Label
+                      htmlFor="in-stock"
+                      className="text-xs font-medium text-gray-600 uppercase"
+                    >
+                      In Stock
+                    </Label>
+                    <Switch className="" id="in-stock" />
+                  </div>
                 </div>
+              </div>
 
-                {/* Product Images */}
-                <div>
-                  <Label className="text-xs font-medium uppercase text-muted-foreground">
-                    Product Images
-                  </Label>
+              <div className="w-full p-4 mt-4 border">
+                <h3 className="text-lg font-semibold">Product Organization</h3>
 
-                  <ImageUpload />
+                <div className="mt-6">
+                  <div className="flex flex-col space-y-4">
+                    <CategoryDropDown />
+                    {/* <TagDropDown /> */}
+                  </div>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
-
-          <div className="col-span-1">
-            <div className="w-full p-4 border">
-              <h3 className="text-lg font-semibold">Pricing</h3>
-            </div>
-          </div>
-        </div>
+        </form>
       </PageContentLayout>
     </div>
   );
---
+++ apps/web/src/components/dashboard/common/CategoryDropDown.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import React, { FC } from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "ui/lib/utils";
+import { Label } from "ui";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "ui";
+import { BsPlug, BsBook, BsPlusLg } from "react-icons/bs";
+import { MdComputer, MdOutlineKitchen, MdOutlineToys } from "react-icons/md";
+import { GiLipstick, GiClothes } from "react-icons/gi";
+import { MdSportsSoccer } from "react-icons/md";
+import { IoCarSportOutline, IoGameControllerOutline } from "react-icons/io5";
+import { BsChevronRight } from "react-icons/bs";
+import { ScrollArea } from "ui";
+
+interface CategoryDropDownProps {}
+const categoriesData = [
+  {
+    id: "electronics",
+    name: "Electronics",
+    icon: <BsPlug />,
+    link: "/search/category/electronics",
+  },
+  {
+    id: "computers",
+    name: "Computers",
+    icon: <MdComputer />,
+    link: "/search/category/computers",
+  },
+  {
+    id: "home",
+    name: "Home & Kitchen",
+    icon: <MdOutlineKitchen />,
+    link: "/search/category/home",
+  },
+  {
+    id: "toys",
+    name: "Toys & Games",
+    icon: <MdOutlineToys />,
+    link: "/search/category/toys",
+  },
+  {
+    id: "books",
+    name: "Books",
+    icon: <BsBook />,
+    link: "/search/category/books",
+  },
+  {
+    id: "beauty",
+    name: "Beauty & Health",
+    icon: <GiLipstick />,
+    link: "/search/category/beauty",
+  },
+  {
+    id: "clothing",
+    name: "Clothing",
+    icon: <GiClothes />,
+    link: "/search/category/clothing",
+  },
+  {
+    id: "sports",
+    name: "Sports & Outdoors",
+    icon: <MdSportsSoccer />,
+    link: "/search/category/sports",
+  },
+  {
+    id: "automotive",
+    name: "Automotive",
+    icon: <IoCarSportOutline />,
+    link: "/search/category/automotive",
+  },
+  {
+    id: "gaming",
+    name: "Gaming & Consoles",
+    icon: <IoGameControllerOutline />,
+    link: "/search/category/gaming",
+  },
+  {
+    id: "more",
+    name: "More Categories",
+    icon: <BsPlusLg />,
+    link: "/search/category/all",
+  },
+];
+
+const CategoryDropDown: FC<CategoryDropDownProps> = () => {
+  return (
+    <div>
+      <Label className="text-xs font-medium text-gray-600 uppercase">
+        Category
+      </Label>
+
+      <Select>
+        <SelectTrigger className="w-full rounded">
+          <SelectValue
+            placeholder="Select a category"
+            className="placeholder:text-muted-foreground text-muted-foreground"
+          />
+        </SelectTrigger>
+        <SelectContent className="overflow-y-auto h-60">
+          <ScrollArea className="h-60">
+            {categoriesData.map((category) => (
+              <SelectItem
+                key={category.id}
+                value={category.id}
+                className="flex flex-row items-center justify-between"
+              >
+                <div className="flex flex-row items-center space-x-4">
+                  {category.icon}
+                  <span>{category.name}</span>
+                </div>
+              </SelectItem>
+            ))}
+          </ScrollArea>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default CategoryDropDown;
